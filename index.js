87b2da9a96c535abde40b9a41232707e7206a619
nj10deadend <nasjacks10@gmail.com>
2021-07-19 17:14:41 -0400

    commit tree fc97097bd332631b3ab97e539d3a518275ee9ed3

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){ // Write your code here!
+  let greeting;
+  if (feet <= 400) {
+    greeting = "This one is on me!";
+  } else if (feet >= 2000 & feet <=2499) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (feet >= 2500) {
+    greeting = 'No can do.'
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {  // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){  // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default: 
+    return "Bye."; 
+  }
+
+  
 }
